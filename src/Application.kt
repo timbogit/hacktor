9d918b8a4f892082ca20d4aa49c7859bcd83f1ca
Tim Schmelmer <tim.schmelmer@gmail.com>
2020-02-06 23:03:32 -0700

    Adding /textToSpeech/{name} endpoint

--- src/Application.kt
+++ src/Application.kt
@@ -59,6 +59,8 @@ fun Application.module(testing: Boolean = false) {
         get("/json/gson") {
             call.respond(mapOf("hello" to "world"))
         }
+
+        textToSpeech()
     }
 }
 
---
+++ src/TextToSpeech.kt
@@ -0,0 +1,46 @@
+package com.salesforce.trailhead
+
+import io.ktor.application.*
+import io.ktor.http.content.*
+import io.ktor.locations.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.http.*
+
+import com.amazonaws.services.polly.AmazonPollyClientBuilder
+import com.amazonaws.services.polly.model.OutputFormat
+import com.amazonaws.services.polly.model.SynthesizeSpeechRequest
+import com.amazonaws.services.polly.model.VoiceId
+import java.io.File
+import java.io.FileOutputStream
+
+@Location("/textToSpeech/{name}")
+data class TextSpeech(val name: String)
+
+var client = AmazonPollyClientBuilder.defaultClient()
+
+fun Route.textToSpeech() {
+    get<TextSpeech> {
+        val outputFileName = "/tmp/speech.mp3"
+        val synthesizeSpeechRequest = SynthesizeSpeechRequest()
+            .withOutputFormat(OutputFormat.Mp3)
+            .withVoiceId(VoiceId.Joanna)
+            .withText("This is a sample text to be synthesized for ${it.name}.")
+        try {
+            FileOutputStream(File(outputFileName)).use { outputStream ->
+                val synthesizeSpeechResult = client.synthesizeSpeech(synthesizeSpeechRequest)
+                val buffer = ByteArray(2 * 1024)
+                var readBytes: Int
+                synthesizeSpeechResult.audioStream.use { `in` ->
+                    while (`in`.read(buffer).also { readBytes = it } > 0) {
+                        outputStream.write(buffer, 0, readBytes)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            System.err.println("Exception caught: $e")
+        }
+        val type = ContentType.fromFilePath(outputFileName).first { it.contentType == "audio" }
+        call.respond(LocalFileContent(File(outputFileName), contentType = type))
+    }
+}
