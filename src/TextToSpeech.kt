bea89f07f0c757b6ff62fecf1b8d71c8af44a512
Tim Schmelmer <tim.schmelmer@gmail.com>
2020-02-06 23:30:27 -0700

    Changing over to an envvar based credentialling

--- src/TextToSpeech.kt
+++ src/TextToSpeech.kt
@@ -11,13 +11,14 @@ import com.amazonaws.services.polly.AmazonPollyClientBuilder
 import com.amazonaws.services.polly.model.OutputFormat
 import com.amazonaws.services.polly.model.SynthesizeSpeechRequest
 import com.amazonaws.services.polly.model.VoiceId
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider
 import java.io.File
 import java.io.FileOutputStream
 
 @Location("/textToSpeech/{name}")
 data class TextSpeech(val name: String)
 
-var client = AmazonPollyClientBuilder.defaultClient()
+var client = AmazonPollyClientBuilder.standard().withCredentials(EnvironmentVariableCredentialsProvider()).build()
 
 fun Route.textToSpeech() {
     get<TextSpeech> {
